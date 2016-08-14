decfd338be8aea066f4af8b3dcba10d5c7f7869e
Tim Akhmedov <kris0108239@gmail.com>
2016-08-14 20:49:06 +0400

    small fixes in common classes, dont know why

--- common/vertex.py
+++ common/vertex.py
@@ -1,3 +1,6 @@
+from .mark import Mark
+
+
 class Vertex(object):
     def __init__(self, name):
         self.name = name
@@ -8,7 +11,7 @@ class Vertex(object):
         self.edges_list.append(edge)
 
     def __repr__(self):
-        return '|name: {}, edges_list: {}, mark: {}|'.format(self.name, self.edges_list, str(self.mark))
+        return 'name: {}, edges_list: {}, mark: {}\n'.format(self.name, self.edges_list, str(self.mark))
 
     def __str__(self):
         return self.__repr__()
